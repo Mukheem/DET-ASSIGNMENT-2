aa4fc2ebf6e9982e20cc0e4bf162cc552a5d08d8
Abdul Mukheem Shaik <mukheemuddin@gmail.com>
2024-02-09 18:25:58 +0100

    Placements without canvas is all good

--- Assets/Scripts/Plank.cs
+++ Assets/Scripts/Plank.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Plank : MonoBehaviour
 {
     public int objectCounter = 0;
+    public Oculus.Interaction.Grabbable plankGrabbableScript;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        //Gets the Plank's Grabbale component
+        plankGrabbableScript = gameObject.GetComponent<Oculus.Interaction.Grabbable>();
+        plankGrabbableScript.enabled = false; // Disable the Plank's grabbale component so that it is immovable
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // when object counter is 5 then the user should be able to move the plank
+        if (objectCounter == 5)
+        {
+            plankGrabbableScript.enabled = true;
+        }
     }
+
+   
 
     public void OnTriggerEnter(Collider other)
     {
